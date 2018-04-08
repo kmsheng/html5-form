8604cfe1b125dcfaf8dc0af854a870bb12534bde
kmsheng <kmsh3ng@gmail.com>
2018-04-08 16:35:45 +0800

    practice constraint validation process

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './index.css';
 
-console.log('Hello World');
+const form = document.getElementById('form');
+const inputName = document.getElementById('name');
+const inputEmail = document.getElementById('email');
+const inputPhone = document.getElementById('phone');
+
+const inputs = [inputName, inputEmail, inputPhone];
+
+form.addEventListener('submit', (event) => {
+
+  event.preventDefault();
+
+  console.log('form.checkValidity()', form.checkValidity());
+  // console.log('form.reportValidity()', form.reportValidity());
+
+  inputs.forEach(input => {
+    console.log(`${input.id}.validity`, inputName.validity);
+    console.log(`${input.id}.validationMessage`, inputName.validationMessage);
+    console.log(`${input.id}.checkValidity`, inputName.checkValidity());
+  });
+}, false);
