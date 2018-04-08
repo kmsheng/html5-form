4ce44c307e9518668106eec774bb10809540b402
kmsheng <kmsh3ng@gmail.com>
2018-04-08 16:09:31 +0800

    set publicPath to dist

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/assets/",
+    publicPath: "/dist/",
     filename: "bundle.js"
   },
   module: {
