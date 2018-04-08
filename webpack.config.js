b255a9471a17b3342db61bb4875b2a461d3b2090
kmsheng <kmsh3ng@gmail.com>
2018-04-08 15:47:57 +0800

    rename output path from build -> dist

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     app: ["./src/index.js"]
   },
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
     publicPath: "/assets/",
     filename: "bundle.js"
   }
