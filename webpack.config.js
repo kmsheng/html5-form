c4a0999c351d2572d71e4e9e314c50cbcda28409
kmsheng <kmsh3ng@gmail.com>
2018-04-08 15:44:56 +0800

    add file webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+module.exports = {
+  entry: {
+    app: ["./src/index.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
+  }
+};
